139f19972b421129dfbb2e02ba2ae5b44fd91211
LiveSparks <livesparking@gmail.com>
2016-04-19 14:15:07 +0530

    Added files via upload

---
+++ ESP_WiFi_Switch.ino
@@ -0,0 +1,113 @@
+/*
+ *  This sketch will set up a simple HTTP-like server to control the gpio pin 2 of the esp8266.
+ *  The server will set a GPIO pin depending on the request
+ *    http://server_ip/gpio/0 will set the GPIO2 low,
+ *    http://server_ip/gpio/1 will set the GPIO2 high
+ *  server_ip is the IP address of the ESP8266 module, will be 
+ *  printed to Serial when the module is connected.
+ */
+
+#include <ESP8266WiFi.h>
+
+const char* ssid = "ssid";              //Replace <ssid> with the SSID of your WiFi Network e.g. const char* ssid = "YourWifi";
+const char* password = "pass";          //Replace <pass> with the password of your WiFi Network e.g. const char* password = "YourPassword";
+
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // prepare GPIO2
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  WiFi.config(192.168.1.7);    //This gives your ESP an static IP address (You can change the ip to your liking)
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+  
+  // Match the request
+  int val;
+  int choice;
+  String s;
+  
+  choice = 0;
+  if (req.indexOf("/gpio/0") != -1)
+    val = 0;
+  else if (req.indexOf("/gpio/1") != -1)
+    val = 1;
+  else if (req.indexOf("/status") != -1)
+    choice = 1;
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  
+  
+  client.flush();
+
+  // Prepare the response
+
+  if (choice == 1){                    // responds with current status of the pin
+    s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n";
+    s += (val)?"high":"low";
+    s += "</html>\n";
+  }
+  else {
+    digitalWrite(2, val);         // Set GPIO2 according to the request
+    s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
+    s += (val)?"high":"low";
+    s += "</html>\n";
+  }
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+}
+
